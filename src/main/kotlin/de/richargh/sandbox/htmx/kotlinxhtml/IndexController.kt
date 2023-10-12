66b86dc97a536a77a21c3fa09c7f0ed4108e4a3d
Richard Gross <richard.gross@maibornwolff.de>
2023-10-12 15:12:54 +0200

    r: remove unused import

--- src/main/kotlin/de/richargh/sandbox/htmx/kotlinxhtml/IndexController.kt
+++ src/main/kotlin/de/richargh/sandbox/htmx/kotlinxhtml/IndexController.kt
@@ -3,7 +3,6 @@ package de.richargh.sandbox.htmx.kotlinxhtml
 import de.richargh.sandbox.htmx.kotlinxhtml.commons.html.redirect
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.servlet.view.RedirectView
 
 @Controller
 class IndexController {
